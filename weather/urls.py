f9a7ec3e26b6c4d757461743fe61850efc8c7930
zeshan <xeshan.ahmed@gmail.com>
2023-03-01 16:01:05 +0500

    index view fixes

--- weather/urls.py
+++ weather/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-	path('', views.index),
+	path(' ', views.index),
 	path('url-fetch-data/',views.word_frequencies),
 	path('integer-data/',views.integer_data),
 ]
